d0275d7d74b88ee9831eae7d400d8bf2b1eaac19
Nancho <her.dar.ferre@gmail.com>
2019-11-18 18:18:54 -0300

    Comenzando comunicacion con el ADE7953

--- msp430x2xx/C-source/msp430x2xx_fet_1.c
+++ msp430x2xx/C-source/msp430x2xx_fet_1.c
@@ -1,8 +1,8 @@
 //******************************************************************************
-//  MSP430x2xx Demo - Software Toggle P1.0
+//  MSP430F2618 - Software Medidor Alterna Proser
 //
-//  Description; Toggle P1.0 by xor'ing P1.0 inside of a software loop.
-//  ACLK = n/a, MCLK = SMCLK = default DCO
+//  Description; ADE7953 measurement.
+//  ACLK = 16MHza, MCLK = SMCLK = 1MHz DCO
 //
 //                MSP430x2xx
 //             -----------------
@@ -10,9 +10,12 @@
 //          | |                 | 16MHz
 //          --|RST          XOUT|--
 //            |                 |
-//            |             P1.0|-->LED
+//            |                 |
+//            |     P3.4/UCA0TXD|------------>
+//            |                 | 4800 - " 8N1 "     1bit c/ 208,33333us
+//            |     P3.5/UCA0RXD|<------------
 //
-//  A. Dannenberg
+//  
 //  Texas Instruments, Inc
 //  January 2006
 //  Built with IAR Embedded Workbench Version: 3.40A
@@ -29,42 +32,64 @@ void Set_DCO_1MHzstored(void);
 volatile unsigned int i;
 
 static volatile uint32_t ms_ticks = 0;
+static volatile uint32_t ms4_ticks = 0;
 static volatile bool tick_1ms_elapsed = false;
-static volatile bool tick_1000ms_elapsed = false;
 static volatile bool tick_500ms_elapsed = false;
 static volatile bool tick_200ms_elapsed = false;
 static volatile bool tick_100ms_elapsed = false;
 
+static volatile bool tick1_4ms_elapsed = false;
+static volatile bool tick1_1000ms_elapsed = false;
 
-void Roca_1(void)
+
+void Time_Handler_2(void)
 {
-    P4OUT ^= 0xFF;                           // Toggle P4.0
-    //CCR0 += 400;                            // Add Offset to CCR0
-
+    ms4_ticks++;
+    tick1_4ms_elapsed = true;  // 4 ms
+    if (ms4_ticks % 250 == 0) {
+        tick1_1000ms_elapsed = true; // 1000 ms
+        ms4_ticks=0;
+        }
+    CCR1+=64000;
 }
 
 /* (1/32768)s=0,00003051757812s -> (1/f)* 33 s =1,007080078125 ms */
-void Time_Handler(void)
+void Time_Handler_1(void)
 {
     ms_ticks++;
     tick_1ms_elapsed = true;  //1ms
 
     if (ms_ticks % 500 == 0) {
         tick_500ms_elapsed = true; //500ms
-        }
-
-    if (ms_ticks % 1000 == 0) {
-        tick_1000ms_elapsed = true; //1000ms
         ms_ticks=0;
-    }
+        }
+    CCR0+=16000;
 }
 
-//Interrupción TIMERA
-#pragma vector=TIMERA0_VECTOR
-__interrupt void Timer_A (void)
-{
-  Time_Handler();
 
+//TIMERA1_VECTOR      (24 * 2u) /* 0xFFF0 Timer A CC1-2, TA */
+//TIMERA0_VECTOR      (25 * 2u) /* 0xFFF2 Timer A CC0 */
+
+#pragma vector=TIMERA0_VECTOR
+__interrupt void Timer_A_0 (void)
+{
+  Time_Handler_1();
+
+}
+
+#pragma vector=TIMERA1_VECTOR
+__interrupt void Timer_A_1 (void)
+{
+  if (TAIV==TA0IV_TACCR1){
+    
+      Time_Handler_2();
+  }
+
+  if (TAIV==TA0IV_TAIFG){
+      //timeroverflow();
+      }
+  
+  
 }
 
 
@@ -72,7 +97,7 @@ int main(void)
 {   
     WDTCTL = WDTPW+WDTHOLD;               // Stop watchdog timer
     for (i = 0; i < 0xfffe; i++);             // Delay for XTAL stabilization
-  
+     
     Set_DCO_1MHzstored();
 //    Set_DCO_using32kHz();
     
@@ -96,33 +121,48 @@ int main(void)
     }
     
     //P1DIR |= 0x01;                            // P1.0 output
-    CCTL0 = CCIE;                             // CCR0 interrupt enabled
-    CCR0 =16000; //65535;
-    //TACTL = TASSEL_2 + MC_2;                  // SMCLK, contmode
-    //TACTL = TASSEL_1 + MC_2;                  // ACLK, contmode
-    //TACTL = TASSEL_2 + MC_1;                  // SMCLK, upmode
-    TACTL = TASSEL_1 + MC_1;                  // ACLK, upmode
+    CCR0 =16000; //65535; 1 ms
+    CCR1 =64000; //65535; 4 ms
+    
+    CCTL0=0;
+    CCTL1=0;
+    
+    CCTL0 = CCIE;       // CCR0 interrupt enabled + compare mode
+    CCTL1 = CCIE;       // CCR1 interrupt enabled + compare mode
+    
+    
+    //TACTL = TASSEL_2 + MC_2;                 // SMCLK, contmode
+    TACTL = TASSEL_1 + MC_2+TAIE;              // ACLK, contmode,TAIFG interrupt request
+    //TACTL = TASSEL_2 + MC_1;                 // SMCLK, upmode
+    //TACTL = TASSEL_1 + MC_1+ TAIE;           // ACLK, upmode, TAIFG interrupt request
    
 
 /*Entra en el bucle de las tasks*/
     //__bis_SR_register(LPM0_bits + GIE);       // Enter LPM0 w/ interrupt    
    
-    __bis_SR_register(GIE);       //interrupt    
-    while (1) 
+    __bis_SR_register(GIE);       //interrupt
+    //bool b = 1;
+    while (1) //milisecond controlled Loop
         {
           
         if (tick_1ms_elapsed) {
             //P4OUT ^= 0xFF;
             tick_1ms_elapsed = false; // Reset the flag (signal 'handled')
         }
+        
+        if (tick1_4ms_elapsed) {
+            //P4OUT ^= 0xFF;
+            tick1_4ms_elapsed = false; // Reset the flag (signal 'handled')
+        }
+        
         if (tick_500ms_elapsed) {
-            P4OUT ^= 0xFF; 
+            P4OUT^= 0xF0;
             tick_500ms_elapsed = false; // Reset the flag (signal 'handled')
         }
 
-        if (tick_1000ms_elapsed) {
-            //P4OUT ^= 0xFF; 
-            tick_1000ms_elapsed = false; // Reset the flag (signal 'handled')
+        if (tick1_1000ms_elapsed) {
+            P4OUT^= 0x0F;
+            tick1_1000ms_elapsed = false; // Reset the flag (signal 'handled')
         }
 
         
@@ -130,7 +170,21 @@ int main(void)
      //__delay_cycles(9000000);
     //TACTL=MC_0;
     // __bis_SR_register(LPM0_bits);
+    //No Llega aqui
         
+}
+void serial_config (void){
+
+    P3SEL = 0x30; 
+    UCA0CTL1 |= UCSSEL_2; // SMCLK  16 MHz = 2 MHz
+    /*(UCAxBR0 + UCAxBR1 × 256) = Baud Rate*/ 
+    UCA0BR0 = 8;                              // 1MHz 115200
+    UCA0BR1 = 0;                              // 1MHz 115200
+    UCA0MCTL = UCBRS2 + UCBRS0;               // Modulation UCBRSx = 5
+}
+
+void Lectura_ADE795 (void){
+ 
 }
 
 void Set_DCO_1MHzstored(void){
@@ -159,11 +213,12 @@ void Set_DCO_1MHzstored(void){
     BCSCTL1=CALBC1_1MHZ;
     DCOCTL=CALDCO_1MHZ;
 
-    BCSCTL2 |= SELM_0 + DIVM_0;   //This statement chooses (SELM_0: 0000 0000) DCO as
-                                  //the source for MCLK and SMCLK with internal
-                                  //resistor. The clock division is by 8, as choosen DIVM_3 (x3).
+    BCSCTL2 |= SELM_0 + DIVM_0 + SELS + DIVS_3 ;
+                             //This statement chooses (SELM_0: 0000 1110) DCO as
+                             //the source for MCLK 
+                             //SMCLK <--  ACKL , SMCLK divider8
+                             //The clock division is by 8 if DIVM_3 (x3) is choosen .
     
-
     BCSCTL1 &= ~(0x03 << 4);  //ACK divider = 1
     BCSCTL1 |= XTS;  //XTS High frecuency
     BCSCTL3 = 0x00;
